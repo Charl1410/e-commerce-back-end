3897dd63d69da935df8fffbb1b07e86c72689ad1
Charl1410 <112945634+Charl1410@users.noreply.github.com>
2023-01-27 17:12:03 +0000

    added findAll get route for categories from db

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -3,9 +3,20 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
+router.get('/', async (req, res) => {
+  try {
+      // find all categories
+
+    const categoryData = await Category.findAll({
   // be sure to include its associated Products
+      include: [
+        {model: Product}
+      ]
+    })
+    res.render(200).json(categoryData);
+  } catch(err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
